4e42ef574e5d1e3a98f666e153aa578544e7e1d1
Izabella Chianca Simonetti <izabellayeh@gmail.com>
2020-07-21 21:07:20 -0300

    Exercicio

---
+++ src/main/java/br/com/bossini/aula07_spring_mvc_boot_hibernate_jpa/Aula07SpringMvcBootHibernateJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.bossini.aula07_spring_mvc_boot_hibernate_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula07SpringMvcBootHibernateJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula07SpringMvcBootHibernateJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bossini/aula07_spring_mvc_boot_hibernate_jpa/Aula07SpringMvcBootHibernateJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bossini.aula07_spring_mvc_boot_hibernate_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula07SpringMvcBootHibernateJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
